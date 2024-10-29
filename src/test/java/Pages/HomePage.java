47756a7690edace4bde3c6390c4c46c93d849c9c
eyasenc <md.eyasenchowdhury@gmail.com>
2024-10-29 23:32:47 +0600

    part 2

--- src/test/java/Pages/HomePage.java
+++ src/test/java/Pages/HomePage.java
@@ -1,4 +1,15 @@
 package Pages;
 
-public class HomePage {
+public class HomePage extends BasePage {
+    public String url = "https://automationexercise.com/";
+    public String title = "Automation Exercise";
+    public By login_signup = By.xpath("//a[contains(text(),'Login / Signup')]");
+    public By logout_button = By.xpath("//a[contains(text(),'Logout')]");
+
+    public void loadHomePage() {
+        loadAWebPage(url);
+
+
+
+    }
 }
--- src/test/java/Pages/LoginPage.java
+++ src/test/java/Pages/LoginPage.java
@@ -1,4 +1,14 @@
 package Pages;
 
 public class LoginPage {
+    public By email_input_box= By.xpath("//input[@name='email']");
+    public By password_input_box= By.xpath("//input[@name='password']");
+    public By login_button= By.xpath("//button[@type='submit']");
+    public By error_message= By.xpath("//div[@class='error']");
+
+    public void navigateToLoginPage(){
+        HomePage homePage= new HomePage();
+        homePage.loadHomePage();
+        homePage.clickOnElement(homePage.login_signup);
+    }
 }
